d8d0b034aaed02aa894699c5ef8dac748332793a
Nick Tikhonov <ndtikhonov@gmail.com>
2025-06-20 17:04:55 +0200

    Name + icon

---
+++ product/painpoints.md
@@ -0,0 +1,3 @@
+# Painpoints
+
+* Jaimin (founder/dev): current workflow involves copy pasting schema into AI tools before generating queries, which is too slow.
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import icon from '../../logo.png?asset'
 import { generateQuery, runQuery, testPostgresConnection } from './lib/db'
 import { getConnectionString, getOpenAiKey, setConnectionString, setOpenAiKey } from './lib/state'
 
@@ -13,7 +13,7 @@ function createWindow(): void {
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon: icon,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: true,
